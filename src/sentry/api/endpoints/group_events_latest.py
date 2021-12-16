c7a2961886f29f6ecf3dc0974987830b8d8425fe
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-12-16 13:13:13 -0800

    Add group type

--- src/sentry/api/endpoints/group_events_latest.py
+++ src/sentry/api/endpoints/group_events_latest.py
@@ -5,11 +5,12 @@ from sentry.api import client
 from sentry.api.bases.group import GroupEndpoint
 from sentry.api.helpers.environments import get_environments
 from sentry.api.helpers.group_index import rate_limit_endpoint
+from sentry.models import Group
 
 
 class GroupEventsLatestEndpoint(GroupEndpoint):
     @rate_limit_endpoint(limit=15, window=1)
-    def get(self, request: Request, group) -> Response:
+    def get(self, request: Request, group: Group) -> Response:
         """
         Retrieve the Latest Event for an Issue
         ``````````````````````````````````````
